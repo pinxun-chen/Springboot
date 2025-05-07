41485e1b11728b2fd2a0b10b56da786bd59b6e76
pinxun-chen <a930406a@gmail.com>
2025-05-07 13:39:23 +0800

    0507

---
+++ springboot-mvc/src/main/java/com/example/demo/controller/ApiController.java
@@ -0,0 +1,80 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // 免去撰寫 @ResponseBody, 但若要透過 jsp 渲染則不適用
+@RequestMapping("/api") // 以下路徑統一都有 URL 前綴 "/api"
+public class ApiController {
+	
+	/**
+	 * 1. 首頁
+	 * 路徑: /home
+	 * 路徑: /
+	 * 網址: http://localhost:8080/api/home
+	 * 網址: http://localhost:8080/api/
+	 * */
+	@GetMapping(value = {"/home", "/"})
+	public String home() {
+		return "我是首頁";
+	}
+	
+	/**
+	 * 2. ?帶參數
+	 * 路徑: /greet?name=John&age=18
+	 * 路徑: /greet?name=Mary
+	 * 網址: http://localhost:8080/api/greet?name=John&age=18
+	 * 結果: Hi John, 18 (成年)
+	 * 網址: http://localhost:8080/api/greet?name=Mary
+	 * 結果: Hi Mary, 0 (未成年)
+	 * 限制: name 參數一定要加, age 為可選參數(有初始值 0)
+	 * */
+	@GetMapping("/greet")
+	public String greet(@RequestParam(value = "name", required = true) String username,
+						@RequestParam(value = "age", required = false, defaultValue = "0") Integer userage) {
+		
+		String result = String.format("Hi %s %d (%s)", 
+				username, userage, userage >= 18 ? "成年" : "未成年");
+		return result;
+	}
+	
+	// 3. 上述 2 的精簡寫法
+	// 方法參數名與請求參數名相同
+	@GetMapping("/greet2")
+	public String greet2(@RequestParam String name,
+						 @RequestParam(defaultValue = "0") Integer age) {
+
+		String result = String.format("Hi %s %d (%s)", 
+								name, age, age >= 18 ? "成年" : "未成年");
+		return result;
+	}
+	
+	/**
+	 * 4. Lab 練習 I
+	 * 路徑: /bmi?h=170&w=60
+	 * 網址: http://localhost:8080/api/bmi?h=170&w=60
+	 * 執行結果: bmi = 20.76
+	 * */
+	
+	@GetMapping("/bmi")
+	public String bmi(@RequestParam(value = "h", required = true) Integer height,
+			          @RequestParam(value = "w", required = true) Integer weight) {
+		double a = height/100.0;
+		double b = weight/(a*a);
+		String bmi = String.format("bmi = %.2f", b);
+		return bmi;
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
