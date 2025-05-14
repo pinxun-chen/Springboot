828d237af0e2451563517746da7dc26ef10e1608
pinxun-chen <a930406a@gmail.com>
2025-05-14 12:09:38 +0800

    0514

--- springboot-mvc/src/main/java/com/example/demo/controller/BookController.java
+++ springboot-mvc/src/main/java/com/example/demo/controller/BookController.java
@@ -4,17 +4,22 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.exception.BookException;
 import com.example.demo.model.Book;
 import com.example.demo.response.ApiResponse;
 import com.example.demo.service.BookService;
+
+import lombok.Delegate;
 
 @RestController
 @RequestMapping("/book")
@@ -27,7 +32,7 @@ public class BookController {
 	public ResponseEntity<ApiResponse<List<Book>>> findAllBooks() {
 		List<Book> books = bookService.findAllBooks();
 		if(books.size() == 0) {
-			return ResponseEntity.badRequest().body(ApiResponse.error("沒有書籍"));
+			return ResponseEntity.badRequest().body(ApiResponse.error("查無此書"));
 		}
 		return ResponseEntity.ok(ApiResponse.success("查詢成功:", books));
 	}
@@ -46,14 +51,31 @@ public class BookController {
 	public ResponseEntity<ApiResponse<Book>> addBook(@RequestBody Book book) {
 		try {
 			bookService.addBook(book);
-			return ResponseEntity.ok(ApiResponse.success("新增成功:", book));
-		}catch (BookException e) {
+			return ResponseEntity.ok(ApiResponse.success("新增成功", book));
+		} catch (BookException e) {
 			return ResponseEntity.badRequest().body(ApiResponse.error(e.getMessage()));
 		}
 	}
 	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<ApiResponse<String>> deleteBook(@PathVariable Integer id) {
+		try {
+			bookService.deleteBook(id);
+			return ResponseEntity.ok(ApiResponse.success("新增成功",""));
+		} catch (BookException e) {
+			return ResponseEntity.badRequest().body(ApiResponse.error(e.getMessage()));
+		}
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<ApiResponse<Book>> updateBook(@PathVariable Integer id, @RequestBody Book book) {
+		try {
+			bookService.updateBook(id, book);
+			return ResponseEntity.ok(ApiResponse.success("修改成功", book));
+		} catch (BookException e) {
+			return ResponseEntity.badRequest().body(ApiResponse.error(e.getMessage()));
+		}
+	}
+	
+	
 }
-
-
-
-
